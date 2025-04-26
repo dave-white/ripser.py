ec177a9d72fe495daf4e35274852cdbcc008ed11
David Graham White <7415754+dave-white@users.noreply.github.com>
2025-04-26 11:39:10 -0500

    Employ struct in input ArgumentList for optional Ripser parameters.  Add documentation (Doxygen).

--- matlab/rips.cpp
+++ matlab/rips.cpp
@@ -2,12 +2,15 @@
  * MEX wrappers for Ripser functions.
  */
 
+#include "MatlabDataArray/StructArray.hpp"
+#include "MatlabDataArray/TypedArrayRef.hpp"
 #include "cppmex/mexMatlabEngine.hpp"
 #include <memory>
+#include <ostream>
 #include <sstream>
+#include <string>
 #include <utility>
 
-#include "mex.hpp"
 #include "mexAdapter.hpp"
 
 #include "ripser.hpp"
@@ -15,62 +18,96 @@
 using namespace matlab::data;
 using matlab::mex::ArgumentList;
 
+/**
+ * @brief
+ */
 class MexFunction : public matlab::mex::Function {
 	ArrayFactory fac;
 	std::shared_ptr<matlab::engine::MATLABEngine> matlab_p = getEngine();
-	std::ostringstream stream;
 
-public:
+	/**
+	 * Print to the MATLAB console.
+	 */
+	void printmex(const std::string &msg)
+	{
+		static std::ostringstream stream;
+		stream << msg << std::endl;
+		matlab_p->feval(
+			u"fprintf", 0,
+			std::vector<Array>({ fac.createScalar(stream.str()) }));
+		stream.str("");
+	}
+
+    public:
+	/**
+	 * @brief Wraps rips_dm.
+	 *
+	 * Invoke rips_dm, computing the persistant homology of the 
+	 * Vietoris-Rips complex
+	 *
+	 * @param[out] outputs List of data objects returned to MATLAB.
+	 * @param[out] outputs[0] Cell array of barcodes.
+	 * @param[out] outputs[1] Cell array of representative cocycles.
+	 * @param[out] outputs[2] Scalar number of edges removed.
+	 * @param[in]  inputs List of arrays holding input arguments from 
+	 * @param[in]  inputs[0] "dist_mat": Flat array containing the entries 
+	 * of the (triangular) distance matrix.
+	 * @param[in]  inputs[1] "opts": Struct holding optional parameters 
+	 * passed to Ripser.
+	 */
 	void operator()(ArgumentList outputs, ArgumentList inputs)
 	{
 		TypedArray<value_t> dist_mat = std::move(inputs[0]);
-		int N = inputs[1][0];
-		int dim_max = inputs[2][0];
-		int modulus = 2;
+
+		/**
+		 * Set default values for Ripser parameters.
+		 *
+		 * @todo Why does do_cocycles = true break Ripser?
+		 */
+		int dim_max = 0;
+		int modulus = 2; // Should be prime?
 		float threshold = -1.0;
-		int do_cocycles = 0;
+		bool do_cocycles = false;
 
-		stream << "Break 1" << std::endl;
-		printmex(stream);
+		/**
+		 * Get Vietoris-Rips params from MATLAB options struct.
+		 *
+		 * @todo Handle nonexistent fields for optional params.
+		 *
+		 * @todo Figure out why type 'Array' works below rather than, 
+		 * say, 'TypedArray<int>'.
+		 */
+		StructArray opts(inputs[1]);
 
-		switch (inputs.size()) {
-		case 6:
-			do_cocycles = inputs[5][0];
+		Array dim_max_mat = opts[0]["dim_max"];
+		dim_max = dim_max_mat[0];
+		
+		Array modulus_mat = opts[0]["modulus"];
+		modulus = modulus_mat[0];
 
-		case 5:
-			threshold = inputs[4][0];
+		// Maximal distance used in the Vietoris-Rips filtration.
+		Array threshold_mat = opts[0]["threshold"];
+		threshold = threshold_mat[0];
 
-		case 4:
-			modulus = inputs[3][0];
+		Array do_cocycles_mat = opts[0]["do_cocycles"];
+		do_cocycles = do_cocycles_mat[0];
 
-		default:
-			break;
-		}
-
-		stream << "Break 2" << std::endl;
-		printmex(stream);
-
-		ripserResults res = rips_dm(dist_mat.release().get(), N,
+		ripserResults res = rips_dm(dist_mat.release().get(),
+					    dist_mat.getNumberOfElements(),
 					    modulus, dim_max, threshold,
 					    do_cocycles);
 
-		stream << "Break 3" << std::endl;
-		printmex(stream);
-
-		outputs[0] = fac.createArray<Array>({1, 
-			res.births_and_deaths_by_dim.size()});
+		outputs[0] = fac.createArray<Array>(
+			{ 1, res.births_and_deaths_by_dim.size() });
 		for (int k = 0; k < res.births_and_deaths_by_dim.size(); k++) {
-		stream << "Break 5.1" << std::endl;
-		printmex(stream);
-
 			outputs[0][k] = fac.createArray(
 				{ 1, res.births_and_deaths_by_dim[k].size() },
 				res.births_and_deaths_by_dim[k].begin(),
 				res.births_and_deaths_by_dim[k].end());
 		}
 
-		outputs[1] = fac.createArray<Array>({1, 
-			res.cocycles_by_dim.size()});
+		outputs[1] = fac.createArray<Array>(
+			{ 1, res.cocycles_by_dim.size() });
 		for (int k = 0; k < res.cocycles_by_dim.size(); k++) {
 			outputs[1][k] = fac.createArray<Array>(
 				{ 1, res.cocycles_by_dim[k].size() });
@@ -84,36 +121,5 @@ public:
 		}
 
 		outputs[2] = fac.createScalar(res.num_edges);
-		return;
-
-		StructArray out = fac.createStructArray(
-			{ 1, 1 }, { "intervals", "cocycles", "edges" });
-
-		stream << "Break 4" << std::endl;
-		printmex(stream);
-
-		out[0]["intervals"] = fac.createArray<Array>(
-			{ 1, res.births_and_deaths_by_dim.size() });
-
-		stream << "Break 5" << std::endl;
-		printmex(stream);
-
-		stream << "Break 6" << std::endl;
-		printmex(stream);
-
-		out[0]["cocycles"] = fac.createArray<Array>(
-			{ 1, res.cocycles_by_dim.size() });
-		stream << "Break 7" << std::endl;
-		printmex(stream);
-
-		out[0]["edges"] = fac.createScalar(res.num_edges);
-
-		outputs[0] = out;
-	}
-
-	void printmex(std::ostringstream& stream) {
-		matlab_p->feval(u"fprintf", 0, std::vector<Array>({ 
-			fac.createScalar(stream.str())}));
-		stream.str("");
 	}
 };
